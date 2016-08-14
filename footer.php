fb61401b918c71e13b90d9c468455c9228874a0a
duhaime <douglas.duhaime@gmail.com>
2016-08-14 00:17:33 +0000

    create footer placeholder with updated design

--- footer.php
+++ footer.php
@@ -13,23 +13,7 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
- 
-      <div class="contact-us-container">
-        <div class="contact-us-blurb">Want to share feedback or connect to our team? Get in touch with us!</div>
-        <div class="contact-button-container">
-          <a href="mailto:marijeta.bozovic@yale.edu">
-            <div class="button contact-us-button">Contact</div>
-          </a>
-        </div>
-      </div>
-
-      <div class="brand-footer-container">
-        <div class="dh-logo-brand">
-
-   </div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <div class="footer-blue"></div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
