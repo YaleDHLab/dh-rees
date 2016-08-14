34a9d04564f17d9174bb9c137cc4754a4dab5657
duhaime <douglas.duhaime@gmail.com>
2016-08-14 00:18:12 +0000

    use functions.php to load custom js files

--- functions.php
+++ functions.php
@@ -104,6 +104,9 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
+ /* Add custom scripts */
+  wp_enqueue_script( 'svg-background', get_template_directory_uri() . '/js/svg-background.js', array ( 'jquery' ), 1.1, false);
+
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
