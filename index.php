09af6341fcdfe0d5917a9bb3e694d2d9812dd4fb
duhaime <douglas.duhaime@gmail.com>
2016-08-22 17:07:31 +0000

    factor contact parallelograms into partial

--- index.php
+++ index.php
@@ -68,16 +68,7 @@ get_header(); ?>
       </div><!-- .showcase-project-container -->
 
       <!-- Parallelograms -->
-      <div class="parallelogram-container">
-        <div class="baby-blue-parallelogram-text">
-      Join our listserve to stay informed about upcoming events, news and opportunities in the field.
-        </div>
-        <div class="button sign-up-button">Sign Up</div>
-        <div class="baby-blue-parallelogram"></div>
-        <div class="gray-parallelogram">
-          <div class="sign-up-gray-image"></div>
-        </div>
-      </div>
+      <?php get_template_part( 'template-parts/contact-parallelograms', 'none' ); ?>
 
       <!-- Mission and Connect -->
       <div class="mission-and-connect-container">
---
+++ template-parts/contact-parallelograms.php
@@ -0,0 +1,10 @@
+<div class="parallelogram-container">
+  <div class="baby-blue-parallelogram-text">
+Join our listserve to stay informed about upcoming events, news and opportunities in the field.
+  </div>
+  <div class="button sign-up-button">Sign Up</div>
+  <div class="baby-blue-parallelogram"></div>
+  <div class="gray-parallelogram">
+    <div class="sign-up-gray-image"></div>
+  </div>
+</div>
