8c88d3e7d4eb530c601468d855f276505085378c
duhaime <douglas.duhaime@gmail.com>
2016-08-22 19:28:08 +0000

    factor featured project div into partial

--- index.php
+++ index.php
@@ -18,26 +18,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
     <!-- Featured project carosel -->
-    <div class="featured-project-container page-lane">
-      <?php $query = new WP_Query( array( 'category_name' => 'featured-project' ) );
-        if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-        <div class="featured-project-text-container">
-          <div class="featured-project-title"><?php the_title(); ?></div>
-            <div class="featured-project-blurb"><?php echo get_post_meta($post->ID, 'project-blurb', true); ?>
-            <div class="featured-project-button-container">
-              <a href="<?php echo esc_url( get_permalink() ); ?>">
-                <div class="button featured-project-button">View Project</div>
-              </a>
-            </div>
-          </div> 
-        </div>
-
-        <div class="featured-project-image-container">
-        <div class="image-stripe featured-image-stripe"></div>
-           <img class="featured-project-image" src="<?php the_post_thumbnail_url('original'); ?>" />
-        </div>
-      <?php endwhile; endif; ?>
-    </div>
+    <?php get_template_part( 'template-parts/featured-project-carousel', 'none' ); ?>
 
     <!-- Showcase project grid -->
     <div class="showcase-project-container">
