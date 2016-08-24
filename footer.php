b0f00e6b177dc2bea35217c30eaa4058b2977436
duhaime <douglas.duhaime@gmail.com>
2016-08-24 16:03:47 +0000

    swap inlined svgs for S3 svgs

--- footer.php
+++ footer.php
@@ -24,48 +24,26 @@
     </div>
 
     <div class="footer-icon-container">
+
       <div class="footer-icon footer-icon-email">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="3991.282 1654 23.343 14.585">
-          <defs>
-            <style>
-              .cls-1 {
-                fill: #fff;
-              }
-            </style>
-          </defs>
-          <g id="group-42" transform="translate(3991.282 1654)">
-            <path id="path" class="cls-1" d="M21.665.044,21.461,0H1.041L.91.029l10.341,9.2Z" transform="translate(0.417)"/>
-            <path id="path-2" data-name="path" class="cls-1" d="M462.223,770.67l-7.293,6.49,7.293,6.345a1.459,1.459,0,0,0,.1-.686V771.151a1.459,1.459,0,0,0-.1-.481Z" transform="translate(-438.988 -769.693)"/>
-            <path id="path-3" data-name="path" class="cls-1" d="M444.117,770.61a1.458,1.458,0,0,0-.117.569v11.668a1.457,1.457,0,0,0,.131.6l7.293-6.286Z" transform="translate(-444 -769.72)"/>
-            <path id="path-4" data-name="path" class="cls-1" d="M11.233,8.541,8.126,5.77.95,11.925l.073.015H21.384L14.339,5.785Z" transform="translate(0.436 2.646)"/>
-          </g>
-        </svg>
+        <object data="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-email.svg"
+              type="image/svg+xml">
+          <img src="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-email.png" />
+        </object>
       </div><!-- .footer-icon (mail) -->
 
       <div class="footer-icon footer-icon-facebook">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="4047 1648 12.146 26.017">
-          <defs>
-            <style>
-              .cls-1 {
-                fill: #fff;
-              }
-            </style>
-          </defs>
-          <path id="path-16" class="cls-1" d="M151.618,127.089v3.577H149v4.341h2.618v13.008h5.4V135.008h3.61s.341-2.1.5-4.39h-4.1v-2.992a1.268,1.268,0,0,1,1.171-1.057h2.943V122H157.13C151.488,122,151.618,126.439,151.618,127.089Z" transform="translate(3898 1526)"/>
-        </svg>
+        <object data="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-fb.svg"
+              type="image/svg+xml">
+          <img src="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-fb.png" />
+        </object>
       </div><!-- .footer-icon (facebook) -->
 
       <div class="footer-icon footer-icon-github">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="4084 1651 38.867 29.814">
-          <defs>
-            <style>
-              .cls-1 {
-                fill: #fff;
-              }
-            </style>
-          </defs>
-          <path id="path-18" class="cls-1" d="M465.369,125.813a6.747,6.747,0,0,1,.34,4.443,9.711,9.711,0,0,1,1.724,9.225,33.552,33.552,0,0,1,8.424.777,38.621,38.621,0,0,0-7.477-.607c-.51,0-1.238,0-1.214.68a40.792,40.792,0,0,1,8.594,1.287,64.011,64.011,0,0,0-8.594-1.117,8.206,8.206,0,0,1-7.283,4.03c.388.825,1.117,1.141,1.287,2.428.267,1.991-.413,4.855.267,6.239.316.631.85.655,1.214,1.19-.874,1.02-3.035,0-3.277-1.19-.413-1.845.631-4.734-.51-6a30.713,30.713,0,0,0,0,6.676c.218.7.874.971.68,1.626-4.03.413-2.428-5.219-3.011-8.545-.607,0-.34.923-.34,1.287,0,3.253.655,7.7-2.841,7.283,0-.68.461-.923.68-1.53.631-1.821,0-4.54.17-6.749-1.335,1.02.146,4.54-.68,6.336a2.573,2.573,0,0,1-3.18,1.02c.146-.777.947-.655,1.287-1.359s0-2.428.17-3.933a4.71,4.71,0,0,1-5.341-1.627,16.587,16.587,0,0,0-1.02-2.136c-.51-.607-1.311-.7-1.554-1.529,3.059-.728,3.18,3.156,5.924,3.253a6.654,6.654,0,0,0,2.064-.413,3.641,3.641,0,0,1,1.384-2.233c-3.423-.461-6.239-1.529-7.574-4.1a46.93,46.93,0,0,0-8.594,1.19,28.3,28.3,0,0,1,8.5-1.287c-.194-1.117-1.821-.8-2.841-.777a47.767,47.767,0,0,0-5.754.607,27.1,27.1,0,0,1,8.157-.68c-.9-2.816-.583-7.283,1.384-8.982a6.116,6.116,0,0,1,.34-4.855,8.885,8.885,0,0,1,4.855,1.966,17,17,0,0,1,5.851-.607c1,0,2.015.486,2.743.437s1.53-.874,2.234-1.19A6.846,6.846,0,0,1,465.369,125.813Z" transform="translate(3647.01 1525.26)"/>
-        </svg>
+        <object data="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-git.svg"
+              type="image/svg+xml">
+          <img src="https://s3-us-west-2.amazonaws.com/dh-rees/images/icon-git.png" />
+        </object>
       </div><!-- .footer-icon (github) -->
     </div><!-- .footer-icon-container -->
   </div><!-- .footer-blue -->
