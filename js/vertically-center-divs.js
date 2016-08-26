c38ef6e2f238a06eb9c82939a12cbdcc4717d3da
duhaime <douglas.duhaime@gmail.com>
2016-08-26 19:12:51 +0000

    update js to only update styles on large viewports

--- js/vertically-center-divs.js
+++ js/vertically-center-divs.js
@@ -1,36 +1,42 @@
 // vertically center the text within the baby blue parallelogram
 var verticallyCenter = function() {
- 
-  // calculate the height of the container
-  var containerHeight = $(".baby-blue-parallelogram").height();
- 
-  // calculate the height of the text to vertically center
-  var text = $(".baby-blue-parallelogram-text");
-  var textHeight = text.height();
-  
-  // calculate and apply the required margin for the join text
-  var textOffset = (containerHeight - textHeight) / 2;
-  text.css({"marginTop": textOffset});
 
-  // calculate and apply the required margin for the sign up button
-  var button = $(".sign-up-button");
-  var buttonHeight = button.height();
-  var buttonPaddingT = parseInt(button.css("paddingTop"), 10);
-  var buttonPaddingB = parseInt(button.css("paddingBottom"), 10);
-  var totalButtonHeight = buttonHeight + buttonPaddingT + buttonPaddingB;
+  // only adjust elements on desktop views
+  var windowWidth = $(window).width();
 
-  var buttonOffset = (containerHeight - totalButtonHeight) / 2;
-  button.css({"marginTop": buttonOffset});
+  if (windowWidth > 1000) {
 
-  // also vertically center the nav links
-  var containerHeight = $(".menu-rees-menu-container").height();
-  var link = $(".menu-item a");
-  var linkHeight = link.height();
-  var requiredTop = (containerHeight-linkHeight)/2;
+    // calculate the height of the container
+    var containerHeight = $(".baby-blue-parallelogram").height();
 
-  link.css({
-    "marginTop": requiredTop
-  });
+    // calculate the height of the text to vertically center
+    var text = $(".baby-blue-parallelogram-text");
+    var textHeight = text.height();
+
+    // calculate and apply the required margin for the join text
+    var textOffset = (containerHeight - textHeight) / 2;
+    text.css({"marginTop": textOffset});
+
+    // calculate and apply the required margin for the sign up button
+    var button = $(".sign-up-button");
+    var buttonHeight = button.height();
+    var buttonPaddingT = parseInt(button.css("paddingTop"), 10);
+    var buttonPaddingB = parseInt(button.css("paddingBottom"), 10);
+    var totalButtonHeight = buttonHeight + buttonPaddingT + buttonPaddingB;
+
+    var buttonOffset = (containerHeight - totalButtonHeight) / 2;
+    button.css({"marginTop": buttonOffset});
+
+    // also vertically center the nav links
+    var containerHeight = $(".menu-rees-menu-container").height();
+    var link = $(".menu-item a");
+    var linkHeight = link.height();
+    var requiredTop = (containerHeight-linkHeight)/2;
+
+    link.css({
+      "marginTop": requiredTop
+    });
+  }
 
 };
 
