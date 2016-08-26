71a1c33438af5760b6b9e12f13b0eab443c51526
duhaime <douglas.duhaime@gmail.com>
2016-08-26 21:32:39 +0000

    make class substitutions more legible

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $(".navigation-dropdown-container").click(function() {
     if ($(".mobile-navigation-menu").hasClass("hidden")) {
-      $(".mobile-navigation-menu").toggleClass("hidden");
+      $(".mobile-navigation-menu").removeClass("hidden");
       $(".mobile-navigation-menu").toggleClass("slideOutRight slideInRight"); 
     } else {
       setTimeout(function() {
-        $(".mobile-navigation-menu").toggleClass("hidden");
+        $(".mobile-navigation-menu").addClass("hidden");
       }, 500);
       $(".mobile-navigation-menu").toggleClass("slideInRight slideOutRight");
     }
