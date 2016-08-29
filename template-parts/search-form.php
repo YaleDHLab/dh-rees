23956c59ffc4d148e28f465a5c2a9e0627c2dbff
duhaime <douglas.duhaime@gmail.com>
2016-08-29 15:45:22 +0000

    check in search form for mobile nav

---
+++ template-parts/search-form.php
@@ -0,0 +1,13 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+  <label>
+    <span class="screen-reader-text">Search for:</span>
+    <input type="search" class="search-field" placeholder="Search" value="" name="s" title="Search" />
+  </label>
+  <input type="submit" class="search-submit" value="Search" />
+  <div class="magnifying-glass">
+    <object data="https://s3-us-west-2.amazonaws.com/dh-rees/images/magnifying-glass.svg" type="image/svg+xml">
+      <img src="https://s3-us-west-2.amazonaws.com/dh-rees/images/magnifying-glass.png" />
+    </object> 
+  </div>
+</form>
+
