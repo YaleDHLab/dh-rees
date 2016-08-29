47194423a1a3eec5071ade3629bc71c80383e0dc
duhaime <douglas.duhaime@gmail.com>
2016-08-29 16:02:37 +0000

    check in script that offers js fallbacks for browsers that don't support css properties

--- functions.php
+++ functions.php
@@ -108,8 +108,9 @@ function _s_scripts() {
   wp_enqueue_script( 'svg-background', get_template_directory_uri() . '/js/svg-background.js', array ( 'jquery' ), 1.1, false);
   wp_enqueue_script( 'join-the-mailing-list', get_template_directory_uri() . '/js/join-the-mailing-list.js', array ( 'jquery' ), 1.1, false);
   wp_enqueue_script( 'vertically-center-divs', get_template_directory_uri() . '/js/vertically-center-divs.js', array ( 'jquery' ), 1.1, false);
-	wp_enqueue_script( 'mobile-menu', get_template_directory_uri() . '/js/mobile-menu.js', array(), '20151215', true );
-	wp_enqueue_script( 'position-vw-divs', get_template_directory_uri() . '/js/position-vw-divs.js', array(), '20151215', true );
+	wp_enqueue_script( 'mobile-menu', get_template_directory_uri() . '/js/mobile-menu.js', array(), 1.1, true );
+	wp_enqueue_script( 'position-vw-divs', get_template_directory_uri() . '/js/position-vw-divs.js', array(), 1.1, true );
+	wp_enqueue_script( 'css-fallbacks', get_template_directory_uri() . '/js/css-fallbacks.js', array(), 1.1, true );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
---
+++ js/css-fallbacks.js
@@ -0,0 +1,31 @@
+// provide fallback styles for browsers that don't support css properties
+var positionDivs = function() {
+
+  // only adjust elements on mobile views
+  var windowWidth = $(window).width();
+
+  if (windowWidth < 1000) {
+
+    /***
+    * Treat rotated elements
+    ***/
+
+    if('transform' in document.body.style) {} else {
+
+      // remove mobile site title background
+      $(".mobile-site-title").css({
+        background: 'none'
+      });
+
+      // remove event stripe
+      $(".event-stripe").css({
+        background: 'none'
+      });
+
+    }
+  };
+};
+
+$(document).ready(positionDivs);
+$(window).on("load resize", positionDivs);
+window.addEventListener("orientationchange", positionDivs);
