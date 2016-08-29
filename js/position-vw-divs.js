1ba33b37703fefb519ed154fc602b79355d22aad
duhaime <douglas.duhaime@gmail.com>
2016-08-29 15:43:50 +0000

    check in util to style assets once styled with css:vw

---
+++ js/position-vw-divs.js
@@ -0,0 +1,41 @@
+// position divs as a function of viewport width
+var positionDivs = function() {
+
+  // only adjust elements on mobile views
+  var windowWidth = $(window).width();
+
+  if (windowWidth < 1000) {
+
+    /***
+    * Treat featured image assets
+    ***/
+
+    var imageHeight = windowWidth * .54;
+
+    // adjust the featured project image
+    var featuredProject = $(".featured-project-image-container");
+    featuredProject.css({
+      height: imageHeight
+    });
+
+    // adjust the showcase projects
+    var showcaseProjects = $(".showcase-project-thumbnail");
+    showcaseProjects.each(function() {
+      $(this).css({
+        height: imageHeight
+      });
+    });
+
+    var subtitles = $(".subtitle-wrapper");
+    subtitles.each(function() {
+      $(this).css({
+        top: imageHeight + 7
+      });
+    });
+
+  };
+};
+
+$(document).ready(positionDivs);
+$(window).on("load resize", positionDivs);
+window.addEventListener("orientationchange", positionDivs);
