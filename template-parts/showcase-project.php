53427401a35deb1521b95ee5facf8b9243992cb9
duhaime <douglas.duhaime@gmail.com>
2016-08-30 17:05:17 +0000

    factor showcase project into partial

---
+++ template-parts/showcase-project.php
@@ -0,0 +1,29 @@
+<div class="showcase-project">
+  <div class='subtitle-wrapper landing-page-subtitle subtitle-1'>
+    <div class="subtitle">
+      <?php echo $showcase_section_subtitle; ?>
+    </div>
+  </div>
+
+  <a href="#<?php echo esc_url( get_permalink() ); ?>">
+    <?php
+      $event_month = get_post_meta($post->ID, 'event-month', true);
+      $event_day = get_post_meta($post->ID, 'event-day', true);
+      if ( strlen($event_month) == 3 && strlen($event_day) > 0) {
+        echo '<div class="image-stripe event-stripe"></div>';
+        echo '<div class="event-stripe-text">';
+          echo '<div class="event-stripe-month">'.$event_month.'</div>';
+          echo '<div class="event-stripe-day">'.$event_day.'</div>';
+        echo '</div>';
+      } else {};
+      unset($event_month);
+      unset($event_day);
+    ?>
+    <div class="showcase-project-thumbnail">
+      <img src="<?php the_post_thumbnail_url('large'); ?>"/>
+    </div>
+    <div class="showcase-project-title"><?php the_title(); ?></div>
+    <div class="showcase-project-blurb">
+      <?php echo get_post_meta($post->ID, 'project-blurb', true); ?></div>
+  </a>
+</div><!-- .showcase-project -->
