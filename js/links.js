72b5204d5b4b17d799faedc30423b2c12c14bd12
duhaime <douglas.duhaime@gmail.com>
2016-08-31 22:12:32 +0000

    check in links

--- js/links.js
+++ js/links.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-  $(".mobile-site-title").click(function() {
-    window.location.replace("http://web.library.yale.edu/dhlab");
-  });
-  $(".navigation-brand > svg").click(function() {
+  /* Mobile links to DHLab and site home */
+  $(".mobile-navigation-brand-link").click(function() {
     window.location.replace("http://web.library.yale.edu/dhlab");
   });
   $(".mobile-site-title-link").click(function() {
     window.location.replace("http://dhrees.yale.edu");
+  });
+  /* Desktop links to DHLab and site home */
+
+
+  $(".navigation-brand > svg").click(function() {
+    window.location.replace("http://web.library.yale.edu/dhlab");
   });
   $(".header-site-title-link").click(function() {
     window.location.replace("http://dhrees.yale.edu");
