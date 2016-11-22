f35e80f56c42881ee5b2205bcc43b327cfa6b7d8
duhaime <douglas.duhaime@gmail.com>
2016-11-22 16:31:17 +0000

    move styles from js to css

--- js/vertically-center-divs.js
+++ js/vertically-center-divs.js
@@ -6,27 +6,6 @@ var verticallyCenter = function() {
 
   if (windowWidth >= 1000) {
 
-    // calculate the height of the container
-    var containerHeight = $(".baby-blue-parallelogram").height();
-
-    // calculate the height of the text to vertically center
-    var text = $(".baby-blue-parallelogram-text");
-    var textHeight = text.height();
-
-    // calculate and apply the required margin for the join text
-    var textOffset = (containerHeight - textHeight) / 2;
-    text.css({"marginTop": textOffset});
-
-    // calculate and apply the required margin for the sign up button
-    var button = $(".sign-up-button");
-    var buttonHeight = button.height();
-    var buttonPaddingT = parseInt(button.css("paddingTop"), 10);
-    var buttonPaddingB = parseInt(button.css("paddingBottom"), 10);
-    var totalButtonHeight = buttonHeight + buttonPaddingT + buttonPaddingB;
-
-    var buttonOffset = (containerHeight - totalButtonHeight) / 2;
-    button.css({"marginTop": buttonOffset});
-
     // also vertically center the nav links
     var containerHeight = $(".navigation-blue").height();
     var link = $(".menu-item a");
