8ef91b98ca498bbc6a8b1243c2b8b7c1a9d7079e
duhaime <douglas.duhaime@gmail.com>
2016-11-23 18:07:02 +0000

    add project card template

---
+++ template-parts/project-card.php
@@ -0,0 +1,17 @@
+<a href="<?php the_permalink() ?>" rel="bookmark">
+  <div class="project-card">
+    <div class="project-card-image-container">
+      <div class="showcase-project-thumbnail">
+        <img src="<?php the_post_thumbnail_url('large'); ?>"/>
+      </div>
+    </div><!--.project-card-image-container-->
+    <div class="project-card-text-container">
+      <div class="project-card-hover-strip"></div>
+      <div class="project-card-title"><?php the_title(); ?></div>
+     <div class="project-card-text">
+        <?php echo get_field('blurb'); ?>
+      </div>
+    </div>
+  </div>
+</a>
+
