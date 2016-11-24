4f610eefcb7f029825312c49002fe338e0b9bc0b
duhaime <douglas.duhaime@gmail.com>
2016-11-24 03:42:21 +0000

    format date strings

--- archive-dhrees-event.php
+++ archive-dhrees-event.php
@@ -43,16 +43,65 @@ get_header(); ?>
 
                     if ($event_date) {
                       // dates are stored in yymmdd format
-                      $event_month = substr($event_date, 2, 2);
-                      $event_day = substr($event_date, 4, 2);
-                    
-                      if ( strlen($event_month) == 3 && strlen($event_day) > 0) {
-                        echo '<div class="image-stripe event-stripe"></div>';
-                        echo '<div class="event-stripe-text">';
-                          echo '<div class="event-stripe-month">'.$event_month.'</div>';
-                          echo '<div class="event-stripe-day">'.$event_day.'</div>';
-                        echo '</div>';
-                      };
+                      $event_month = substr($event_date, 4, 2);
+                      $event_day = substr($event_date, 2, 2);
+
+                      // convert the month integer to a string
+                      switch ($event_month) {
+                        case "01":
+                            $month_string = "JAN";
+                            break;
+
+                        case "02":
+                            $month_string = "FEB";
+                            break;
+
+                        case "03":
+                            $month_string = "MAR";
+                            break;
+
+                        case "04":
+                            $month_string = "APR";
+                            break;
+
+                        case "05":
+                            $month_string = "MAY";
+                            break;
+
+                        case "06":
+                            $month_string = "JUN";
+                            break;
+
+                        case "07":
+                            $month_string = "JUL";
+                            break;
+
+                        case "08":
+                            $month_string = "AUG";
+                            break;
+
+                        case "09":
+                            $month_string = "SEP";
+                            break;
+
+                        case "10":
+                            $month_string = "OCT";
+                            break;
+
+                        case "11":
+                            $month_string = "NOV";
+                            break;
+
+                        case "12":
+                            $month_string = "DEC";
+                            break;
+                    }
+                    echo '<div class="image-stripe event-stripe"></div>';
+                      echo '<div class="event-stripe-text">';
+                        echo '<div class="event-stripe-month">'.$month_string.'</div>';
+                        echo '<div class="event-stripe-day">'.$event_day.'</div>';
+                      echo '</div>';
+                      unset($event_date);
                       unset($event_month);
                       unset($event_day);
                     };
