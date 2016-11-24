ff1b97b5460e3e8ae2a3f6ae4fe3d7f9282610d5
duhaime <douglas.duhaime@gmail.com>
2016-11-24 23:44:45 +0000

    clean up related links

--- template-parts/related-links.php
+++ template-parts/related-links.php
@@ -4,9 +4,13 @@
     <div class="related-posts-title">RELATED</div>
     <!-- variable must be called $post -->
     <?php foreach( $post_objects as $post): ?>
-    <?php setup_postdata($post); ?>
-    <a class="related-post" href="#<?php the_permalink(); ?>"><?php the_title(); ?></a>
-  <?php endforeach; ?>
+      <?php setup_postdata($post); ?>
+      <a class="related-post" href="#
+        <?php echo get_post_permalink(); ?>
+        ">
+        <?php the_title(); ?>
+      </a>
+    <?php endforeach; ?>
   <?php wp_reset_postdata(); ?>
   </div>
 <?php endif; ?>
