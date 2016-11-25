d2f541370be356fa4e2868dd1e08df9e9f5dd57c
duhaime <douglas.duhaime@gmail.com>
2016-11-25 16:30:34 +0000

    add links to resource hero assets

--- archive-dhrees-resource.php
+++ archive-dhrees-resource.php
@@ -14,15 +14,26 @@ get_header(); ?>
   <main id="main" class="site-main dhrees-resource-page" role="main">
 
     <?php // pluck off the first resource for the hero
-      $post_count = 0;
-      while ( have_posts() ) : the_post();
-        $post_count += 1;
-        if ($post_count == 1) {
-          $subtitle = "RESOURCE";
-          $button_label = get_field("button_label"); 
-          include(locate_template( 'template-parts/split-hero.php') );
-        } ?>
-    <?php endwhile; ?>
+      $query = new WP_Query( array(
+        'post_type' => 'dhrees-resource',
+        'meta_query' => array(
+          array(
+            'key' => 'featured',
+            'value' => '"featured"',
+            'compare' => 'LIKE'
+          ) ),
+        'posts_per_page' => 1
+      ) );
+
+      if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
+        $the_permalink = get_the_permalink();
+        $subtitle = "RESOURCE";
+        $button_label = get_field("button_label");
+
+        include(locate_template( 'template-parts/split-hero.php') );
+
+        endwhile; endif;
+      wp_reset_postdata(); ?>
 
     <div class="featured-project-container">
       <div class="text-results">
--- template-parts/split-hero.php
+++ template-parts/split-hero.php
@@ -5,17 +5,19 @@
 ?>
 
 <div class="featured-project-container">
-  <div class="featured-project-image-container featured-item-image-container">
-    <div class="featured-project-image featured-item-image"
-      style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)" ></div>
-  </div>
-  <div class="featured-item-text-container">
-    <div class="featured-item-text-wrap">
-      <div class="subtitle"><?php echo $subtitle ?></div>
-      <div class="featured-item-title"><?php the_title(); ?></div>
-      <div class="<?php echo $bottom_left_class.'">'; ?>
-        <?php echo $bottom_left_text ?>
+  <?php if (isset($the_permalink) ) { echo"<a href=".$the_permalink.">"; }; ?>
+    <div class="featured-project-image-container featured-item-image-container">
+      <div class="featured-project-image featured-item-image"
+        style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)" ></div>
+    </div>
+    <div class="featured-item-text-container">
+      <div class="featured-item-text-wrap">
+        <div class="subtitle"><?php echo $subtitle ?></div>
+        <div class="featured-item-title"><?php the_title(); ?></div>
+        <div class="<?php echo $bottom_left_class.'">'; ?>
+          <?php echo $bottom_left_text ?>
+        </div>
       </div>
     </div>
-  </div>
+  <?php if (isset($the_permalink) ) { echo"</a>"; }; ?>
 </div>
