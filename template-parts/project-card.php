09de0ff17a28087a56fb4319e31f3beb1e20f575
duhaime <douglas.duhaime@gmail.com>
2016-12-06 20:00:17 +0000

    use background images

--- template-parts/project-card.php
+++ template-parts/project-card.php
@@ -1,7 +1,7 @@
 <a href="<?php the_permalink() ?>" rel="bookmark">
   <div class="project-card">
     <div class="project-card-image-container">
-      <img src="<?php the_post_thumbnail_url('large'); ?>"/>
+      <div style="background: url(<?php echo the_post_thumbnail_url('large').') no-repeat center center; -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover">'?></div>
     </div><!--.project-card-image-container-->
     <div class="project-card-text-container">
       <div class="project-card-hover-strip"></div>
